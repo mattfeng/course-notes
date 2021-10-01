436a77a816d47f731be4ffa594c728e610e9a8d7
Matthew Feng <mattfeng@mit.edu>
2021-10-01 06:09:38 -0400

    add references

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,32 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 const IndexPage = () => (
-  <h1>hi</h1>
+  <>
+    <div>
+      <h1>5.12</h1>
+      <ul>
+        <li>
+          <Link to="/5.12/unit1">
+            Unit 1 (L1 - L10): Structure, Energy, and Reactivity
+          </Link>
+        </li>
+      </ul>
+    </div>
+
+    <h1>20.110</h1>
+
+    <div>
+      <h1>references</h1>
+      <ul>
+        <li>
+          <Link to="/ref/latex">LaTeX</Link>
+        </li>
+        <li>
+          <Link to="/ref/gatsby">Gatsby</Link>
+        </li>
+      </ul>
+    </div>
+  </>
 )
 
 export default IndexPage
