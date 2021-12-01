7ff4ce1b6b05b610d7ac71965dd3245564a6b2aa
Matthew Feng <mattfeng@mit.edu>
2021-12-01 16:09:52 -0500

    chem

--- src/templates/blogpost.js
+++ src/templates/blogpost.js
@@ -6,36 +6,51 @@ import Chem from "../components/chem"
 import Tag from "../components/tag"
 import { StaticImage } from "gatsby-plugin-image"
 import { Link } from "gatsby"
-import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader";
+import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader"
 
 import "../styles/katex.css"
 import "../styles/main.scss"
 import * as styles from "../styles/notes.module.scss"
 
 require("katex/dist/katex.min.css")
-deckDeckGoHighlightElement();
+deckDeckGoHighlightElement()
 
-const shortcodes = {C: Chem, T: Tag, Link, SI: StaticImage}
+const SN2 = () => {
+  return (
+    <span>
+      S<sub>N</sub>2
+    </span>
+  )
+}
+
+const RA = () => {
+  return <span>&rarr;</span>
+}
+
+const shortcodes = { C: Chem, T: Tag, Link, SI: StaticImage, SN2, RA }
 
 export default function BlogPost({ data }) {
   const post = data.mdx
   return (
     <>
-    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/contrib/mhchem.min.js" integrity="sha384-LIgAiYlGSAdpNC9+YDjDPF6JeS/RRIumtNo0CmyQERZ/+g0h9MbuYQwf/5pQ4Y4M"  crossOrigin="anonymous"></script>
-    <div className={styles.mainContainer}>
-      <h1>{ post.frontmatter.title }</h1>
-      <MDXProvider components={shortcodes}>
-        <MDXRenderer>
-          { post.body }
-        </MDXRenderer>
-      </MDXProvider>
-    </div>
+      <script
+        defer
+        src="https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/contrib/mhchem.min.js"
+        integrity="sha384-LIgAiYlGSAdpNC9+YDjDPF6JeS/RRIumtNo0CmyQERZ/+g0h9MbuYQwf/5pQ4Y4M"
+        crossOrigin="anonymous"
+      ></script>
+      <div className={styles.mainContainer}>
+        <h1>{post.frontmatter.title}</h1>
+        <MDXProvider components={shortcodes}>
+          <MDXRenderer>{post.body}</MDXRenderer>
+        </MDXProvider>
+      </div>
     </>
   )
 }
 
 export const query = graphql`
-  query($slug: String!) {
+  query ($slug: String!) {
     mdx(slug: { eq: $slug }) {
       body
       frontmatter {
