eae686ccaca6d1cd3b728c9d751db704e66c7c8a
Matthew Feng <mattfeng@mit.edu>
2022-02-09 08:17:43 -0500

    module 1 htgaa

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
                 `flex:${_.round(fluidResult.aspectRatio, 2)};`,
             },
           },
-          `gatsby-remark-numbered-footnotes-patched`,
+          `gatsby-remark-numbered-footnotes`,
           `gatsby-remark-copy-linked-files`,
           {
             resolve: `gatsby-remark-highlight-code`,
